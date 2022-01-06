587c77f8ebf7011adf9731a541866738a55f635c
David Horak <info@davidhorak.com>
2022-01-06 12:11:10 -0700

    Feature/exhaustive or (#10)
    * (?) matches any word character (equivalent to [a-zA-Z0-9_])
    
    * bump types
    
    * add exhaustive OR

--- src/core/or.ts
+++ src/core/or.ts
@@ -1,41 +1,53 @@
 import { identity, pipe, tap } from '../internal'
 import { Evaluable, Evaluation } from './Evaluable'
+import { Match } from './Match'
 
 const execute =
-  (operands: Evaluable[], onEvaluation?: Evaluation) => (context: string) => {
+  (operands: Evaluable[], exhaustive: boolean, onEvaluation?: Evaluation) =>
+  (context: string) => {
+    const matches: Match[] = []
+    let result = false
     for (const operand of operands) {
       const evaluated = operand.execute(context, onEvaluation)
       if (evaluated) {
-        return evaluated
+        result = true
+        if (Array.isArray(evaluated)) {
+          matches.push(...evaluated)
+        }
+        if (!exhaustive) {
+          return evaluated
+        }
       }
     }
 
-    return false
+    return matches.length ? matches : result
   }
 
 export const OR = 'OR'
 
-export const or = (...operands: Evaluable[]): Evaluable => {
-  if (operands.length < 2) {
-    throw new Error('logical OR expression must have at least 2 operands')
-  }
+export const or =
+  (exhaustive = false) =>
+  (...operands: Evaluable[]): Evaluable => {
+    if (operands.length < 2) {
+      throw new Error('logical OR expression must have at least 2 operands')
+    }
 
-  return {
-    id: Symbol(OR),
-    kind: OR,
-    descendants: operands,
-    execute: function (context, onEvaluation) {
-      return pipe(
-        execute(operands, onEvaluation),
-        onEvaluation ? tap((result) => onEvaluation(this, result)) : identity
-      )(context)
-    },
-    test: function (context) {
-      return this.execute(context) !== false
-    },
-    toString: (
-      format = (...operands) =>
-        `(${operands.map((operand) => operand.toString()).join(' OR ')})`
-    ) => format(...operands),
+    return {
+      id: Symbol(OR),
+      kind: OR,
+      descendants: operands,
+      execute: function (context, onEvaluation) {
+        return pipe(
+          execute(operands, exhaustive, onEvaluation),
+          onEvaluation ? tap((result) => onEvaluation(this, result)) : identity
+        )(context)
+      },
+      test: function (context) {
+        return execute(operands, false)(context) !== false
+      },
+      toString: (
+        format = (...operands) =>
+          `(${operands.map((operand) => operand.toString()).join(' OR ')})`
+      ) => format(...operands),
+    }
   }
-}
